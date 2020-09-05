972578d6f75a8f4d3566a81e38fd54d21e98737c
ruhulmatho8 <ruhul.amin@ctrends-software.com>
2020-09-06 01:47:49 +0600

    add dart data_structure

---
+++ data_type.dart
@@ -0,0 +1,38 @@
+void main() {
+  //Note: All the data type in dart are object
+  //Therefore, there initial value is by default 'null'
+
+  //Numbers: int
+  int score = 23;
+  var count = 23; //this is inferred as integer automatically by compiler
+
+  //hex value
+  int hexval = 0xe3e3e3e3;
+
+  //Numbers: double
+  double percentage = 93.5;
+  var progress = 93.5;
+
+  //Strings
+  String name = "Ruhul";
+  var man = "Ruhul";
+
+  //Boolean
+  bool isAlive = true;
+  var alive = true;
+
+  //print the value
+  print(score);
+  print(count);
+
+  print(hexval);
+
+  print(percentage);
+  print(progress);
+
+  print(name);
+  print(man);
+
+  print(isAlive);
+  print(alive);
+}
